23e1ec65ebae2d17471e6c580e7dc6ffaa87cf24
Rassvet0420 <finikok@gmail.com>
2020-03-22 23:57:19 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
 exports.min = function min (array) {
-  return 0;
+  let minimum = Math.min.apply(null, array);
+    if (array && array.lenght) {
+        return minimum;
+    } else {
+      return 0
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maximum = Math.max.apply(null, array);
+    if (array && array.lenght) {
+        return maximum;
+    } else {
+      return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let numberSum = 0;
+  if (array && array.lenght) {
+    for (let i = 0; i < array.lenght; i++) {
+    numberSum += array[i];
+  }
+    let average = numberSum / array.lenght;
+    return average;
+    } else {
+      return 0;
+    }
 }
